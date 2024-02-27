b8a28a7f47a59ea2513353c7c82fdb8297831856
rydahn <159046885+rydahn@users.noreply.github.com>
2024-02-26 21:53:01 -0800

    Update CIS129_JesusRamos_Lab5.py

--- CIS129_JesusRamos_Lab5.py
+++ CIS129_JesusRamos_Lab5.py
@@ -4,24 +4,24 @@
 """This program calculates the number of bottles returned for the week and calculates the total payout"""
 
 # Initialize variables
-totalBottles = 0
+total_bottles = 0
 counter = 1
-todayBottles = 0
-totalPayout = 0
-keepGoing = 'y'
+today_bottles = 0
+total_payout = 0
+keep_going = 'y'
 nbr_of_days = 7
 
 # Loop to run program again
-while keepGoing == 'y':
-    totalBottles = 0 # Reset number of bottles per week
+while keep_going == 'y':
+    total_bottles = 0 # Reset number of bottles per week
     for day in range(1, nbr_of_days + 1):
-        todayBottles = float(input(f'Enter number of bottles returned for day #{day}: '))
-        totalBottles += todayBottles
+        today_bottles = float(input(f'Enter number of bottles returned for day #{day}: '))
+        total_bottles += today_bottles
 
 # Calculate number of bottles and payout
-    totalPayout = totalBottles * 0.10
-    print(f'Total number of bottles returned: {totalBottles}')
-    print(f'Total payout: {totalPayout:.2f}')
-    keepGoing = str(input('Do you want another week\'s worth of data? (Enter \'y\' or \'n\')'))
-    if keepGoing != 'y':
+    total_payout = total_bottles * 0.10
+    print(f'Total number of bottles returned: {total_bottles}')
+    print(f'Total payout: {total_payout:.2f}')
+    keep_going = str(input('Do you want another week\'s worth of data? (Enter \'y\' or \'n\')'))
+    if keep_going != 'y':
         break # End loop
